bf06edae9a423dcbe6b71cfe709cc8cd494c1733
Ricky Ding <416640656@qq.com>
2023-01-29 16:39:51 +0800

    。。

---
+++ tesla-coil-calculator/precache-manifest.49a4dfcb8d60058ed232cb4f1e258f14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ace8484442e417abf516621f750cc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40515e359acb9d914601",
+    "url": "/static/css/2.afc662ca.chunk.css"
+  },
+  {
+    "revision": "c9dbced1f6914890b386",
+    "url": "/static/css/main.5c47ee51.chunk.css"
+  },
+  {
+    "revision": "40515e359acb9d914601",
+    "url": "/static/js/2.27cb6259.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.27cb6259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9dbced1f6914890b386",
+    "url": "/static/js/main.98dec487.chunk.js"
+  },
+  {
+    "revision": "1cba36deef3a7d2b858a",
+    "url": "/static/js/runtime-main.00c5e145.js"
+  }
+]);
